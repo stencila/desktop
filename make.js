121c6c6387c5d474661067865387027a4e373501
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-03-15 23:24:50 +1300

    Use bundler to run npm install

--- make.js
+++ make.js
@@ -1,5 +1,7 @@
 /* globals __dirname */
 var b = require('substance-bundler')
+var exec = require('substance-bundler/extensions/exec')
+var path = require('path')
 
 function _copyAssets() {
   b.copy('./src', './app')
@@ -8,6 +10,13 @@ function _copyAssets() {
 
 b.task('clean', () => {
   b.rm('app')
+})
+
+b.task('postinstall', () => {
+  b.exec('npm install', {
+    verbose: true,
+    cwd: path.join(__dirname, 'node_modules', 'stencila')
+  })
 })
 
 b.task('assets', () => {
