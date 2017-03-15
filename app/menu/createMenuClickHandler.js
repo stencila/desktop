b4b47d76ca5e0e1d797f169cbb9dafffc53575b8
Michael Aufreiter <michael@substance.io>
2017-03-16 00:15:00 +1300

    Remove app from Git.

--- app/menu/createMenuClickHandler.js
+++
@@ -1,9 +0,0 @@
-const currentWindow = require('electron').remote.getCurrentWindow()
-/*
-  Sends an event of the given event name to the focused window
-*/
-module.exports = function createMenuHandler(eventName, data) {
-  return function() {
-    currentWindow.send(eventName, data)
-  }
-}
