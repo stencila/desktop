9416d165b83c1aca0fb54d5ad50473153743df68
Michael Aufreiter <michael@substance.io>
2018-03-30 21:43:43 +0200

    Ready-to-use StencilaDesktopApp component.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 const {
-  getQueryStringParam, Component,
-  InMemoryDarBuffer, substanceGlobals,
-  platform, DefaultDOMElement
+  getQueryStringParam,
+  substanceGlobals,
+  platform
 } = window.substance
 
-const { JATSImportDialog } = window.texture
-
 const {
-  Project,
-  setupStencilaContext,
-  StencilaArchive
+  StencilaDesktopApp
 } = window.stencila
 
 const ipc = require('electron').ipcRenderer
@@ -17,10 +13,8 @@ const darServer = require('dar-server')
 const { FSStorageClient } = darServer
 const url = require('url')
 const path = require('path')
-
 const remote = require('electron').remote
 const { shell } = remote
-
 
 // HACK: we should find a better solution to intercept window.open calls
 // (e.g. as done by LinkComponent)
@@ -30,151 +24,13 @@ window.open = function(url /*, frameName, features*/) {
 
 window.addEventListener('load', () => {
   substanceGlobals.DEBUG_RENDERING = platform.devtools
-  App.mount({}, window.document.body)
+  StencilaDesktopApp.mount({
+    archiveId: getQueryStringParam('archiveDir'),
+    ipc,
+    url,
+    path,
+    shell,
+    FSStorageClient,
+    __dirname
+  }, window.document.body)
 })
-
-class App extends Component {
-
-  didMount() {
-    this._init()
-    this._archive.on('archive:changed', this._archiveChanged, this)
-    ipc.on('document:save', () => {
-      this._save()
-    })
-    ipc.on('document:save-as', (event, newArchiveDir) => {
-      this._saveAs(newArchiveDir)
-    })
-    DefaultDOMElement.getBrowserWindow().on('keydown', this._keyDown, this)
-    DefaultDOMElement.getBrowserWindow().on('click', this._click, this)
-  }
-
-  dispose() {
-    // TODO: is it necessary to do ipc.off?
-    // (App component is never unmounted, only the full window is killed)
-  }
-
-  getInitialState() {
-    return {
-      archive: undefined,
-      error: undefined
-    }
-  }
-
-  render($$) {
-    let el = $$('div').addClass('sc-app')
-    let { archive, host, functionManager, engine, error } = this.state
-
-    if (archive) {
-      el.append(
-        $$(Project, {
-          documentArchive: archive,
-          host,
-          functionManager,
-          engine
-        })
-      )
-    } else if (error) {
-      if (error.type === 'jats-import-error') {
-        el.append(
-          $$(JATSImportDialog, { errors: error.detail })
-        )
-      } else {
-        el.append(
-          'ERROR:',
-          error.message
-        )
-      }
-    } else {
-      // LOADING...
-    }
-    return el
-  }
-
-  _init() {
-    let archiveDir = getQueryStringParam('archiveDir')
-    // let isNew = getQueryStringParam('isNew')
-    console.info('archiveDir', archiveDir)
-    let storage = new FSStorageClient()
-    let buffer = new InMemoryDarBuffer()
-    let archive = new StencilaArchive(storage, buffer)
-    this._archive = archive
-    archive.load(archiveDir)
-      .then(() => {
-        this._updateTitle()
-        return setupStencilaContext(archive)
-      }).then(({host, functionManager, engine}) => {
-        this.setState({archive, functionManager, engine, host})
-      })
-      .catch(error => {
-        console.error(error)
-        this.setState({error})
-      })
-  }
-
-  /*
-    We may want an explicit save button, that can be configured on app level,
-    but passed down to editor toolbars.
-  */
-  _save() {
-    this.state.archive.save().then(() => {
-      this._updateTitle(false)
-    }).catch(err => {
-      console.error(err)
-    })
-  }
-
-  _saveAs(newArchiveDir) {
-    console.info('saving as', newArchiveDir)
-    this.state.archive.saveAs(newArchiveDir).then(() => {
-      this._updateTitle(false)
-      ipc.send('document:save-as:successful')
-      // Update the browser url, so on reload, we get the contents from the
-      // new location
-      let newUrl = url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        query: {
-          archiveDir: newArchiveDir
-        },
-        slashes: true
-      })
-      window.history.replaceState({}, 'After Save As', newUrl);
-
-    }).catch(err => {
-      console.error(err)
-    })
-  }
-
-  _archiveChanged() {
-    const hasPendingChanges = this._archive.hasPendingChanges()
-    if (hasPendingChanges) {
-      ipc.send('document:unsaved')
-      this._updateTitle(hasPendingChanges)
-    }
-  }
-
-  _updateTitle(hasPendingChanges) {
-    let newTitle = this._archive.getTitle()
-    if (hasPendingChanges) {
-      newTitle += " *"
-    }
-    document.title = newTitle
-  }
-
-  _keyDown(event) {
-    if ( event.key === 'Dead' ) return
-    // Handle custom keyboard shortcuts globally
-    let archive = this.state.archive
-    if (archive) {
-      let manuscriptSession = archive.getEditorSession('manuscript')
-      manuscriptSession.keyboardManager.onKeydown(event)
-    }
-  }
-
-  _click(event) {
-    if (event.target.tagName === 'A' && event.target.attributes.href.value !== '#') {
-      event.preventDefault();
-      shell.openExternal(event.target.href)
-    }
-  }
-}
